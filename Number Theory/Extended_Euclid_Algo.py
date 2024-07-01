e208608e5eb74eb409c5a112ca24de8a9f8ed854
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-01 14:37:26 +0530

    Extended Euclid's Algorithm

---
+++ Number Theory/Extended_Euclid_Algo.py
@@ -0,0 +1,22 @@
+#To find the multiplicative inverse in modular arithmetic.
+
+#Extended Euclid's Algorithm
+def Extended_Euclid(a, b):
+    t1 = 0
+    t2 = 1
+    while b != 0:
+        t = t1 - (a//b)*t2
+        r = a % b
+        a = b
+        b = r
+        t1 = t2
+        t2 = t
+    else:
+        return t1
+
+#Main
+print("MULTIPLICATIVE INVERSE USING EXTENDED EUCLID'S ALGORITHM")
+print("Constraint: a > b")
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print("Multiplicative inverse of ", b," mod ", a,": ", Extended_Euclid(a, b))
